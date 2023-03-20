5b774cdc3c5ded9cb57119bae5a91033192a5f96
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-03-20 11:28:26 +0700

    comment

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -11,6 +11,8 @@ function App() {
   const [data, setData] = useState([]);
   const [show, setShow] = useState(false);
   const [profile, setProfile] = useState();
+
+  //REDUX
   const fav = useSelector(selectFavorite);
   const dispatch = useDispatch();
 
@@ -24,12 +26,12 @@ function App() {
     let temp = fav;
     if (temp.indexOf(int) !== -1) {
       temp = temp.filter((item) => item !== int);
-      dispatch(addFavorite(temp));
+      dispatch(addFavorite(temp));////////////////REDUX
       alert("unfaved");
     } else {
       temp = temp.filter((item) => typeof item !== String);
       temp.push(int);
-      dispatch(addFavorite(temp));
+      dispatch(addFavorite(temp));////////////////REDUX
       alert("faved");
     }
   };
