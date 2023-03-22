4ba2f20f7cfcd13fecad1af4e5f5bc9568bd36fc
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-03-22 13:07:17 +0700

    redux code structuring

--- src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import favoriteReducer from "../reducer/favoriteSlice";
-
-export default configureStore({
-  reducer: {
-    favorite: favoriteReducer,
-  },
-});
