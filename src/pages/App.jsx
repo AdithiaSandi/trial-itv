8b9b256e9fe9677b871b072b2081a6e2ac72f84e
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-11-22 16:24:32 +0700

    revert again

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,13 +8,13 @@ import { Footer } from '../components/Footer'
 import { ScrollTop } from '../components/ScrollTop'
 import { Teams } from '../components/Teams'
 import { About } from '../components/About'
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 
 const MEASUREMENT_ID = 'G-JGRLH6VWQ1' // YOUR_OWN_MEASUREMENT_ID
 ReactGA.initialize(MEASUREMENT_ID, { debug: true })
 
 function App () {
-  ReactGA.pageview(window.location.pathname + window.location.search)
+  // ReactGA.pageview(window.location.pathname + window.location.search)
   useEffect(() => {
     ReactGA.send('pageview')
   }, [])
