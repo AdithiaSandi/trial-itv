fb78740a608655db33ec1c3465172378cb3983e2
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-11-22 16:43:56 +0700

    yeet

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,7 +8,7 @@ import { Footer } from '../components/Footer'
 import { ScrollTop } from '../components/ScrollTop'
 import { Teams } from '../components/Teams'
 import { About } from '../components/About'
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 
 const MEASUREMENT_ID = 'G-JGRLH6VWQ1' // YOUR_OWN_MEASUREMENT_ID
 ReactGA.initialize(MEASUREMENT_ID, { debug: true })
